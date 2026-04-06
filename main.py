14657f5c83c3b2b1d3d858919536c46ad85ce213
Shreeya P Bambhoray <shreeyapbambhoray@gmail.com>
2026-04-06 12:33:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html', title='Welcome Page')
+    return render_template('index.html', title='Home Page')
+if __name__=='__main__':
+    app.run(host='127.0.0.1',port=8080,debug=True)
